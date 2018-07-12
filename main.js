5261c9bda7d262c1bfa0664b702ee9ba82d4197f
John Carlisle <jmcarlisle@live.com>
2018-07-11 22:37:37 -0700

    commented out main.js  to make it easier to understand - John Carlisle

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var currentColumn = null;
 var possibleRow = null;
 var possibleColumn = null;
 var currentPlayer = 0;
-var gameboard = [
+var gameBoard = [
     [0,1,0,1,0,1,0,1],
     [1,0,1,0,1,0,1,0],
     [0,1,0,1,0,1,0,1],
@@ -30,6 +30,7 @@ var gameboard = [
   ];
 
 function switchPlayer () {
+    // if current player is true, make current player a false value, change the text in the header to message
     if(currentPlayer) {
         currentPlayer = 1 - currentPlayer;
         $("h1").text("Player 2 Turn");
@@ -40,64 +41,48 @@ function switchPlayer () {
 }
 
   function createBoard(){
-    var gameBoard = $('#gameArea');
+    //assign the alternateColor a false value
     var alternateColor = 0;
-    for ( var i = 0; i < gameboard.length; i++) {
+    //loop the number of rows needed, dynamically create a div with the class of row
+    for ( var i = 0; i < gameBoard.length; i++) {
        var row = $('<div>').addClass('row');
-        for( var j = 0; j < gameboard.length; j++) {
-          if( i < 4) {
+       //loop the number of game squares needed per row, 
+        for( var j = 0; j < gameBoard.length; j++) {
+            // if alternateColor is a truthy value, create a div with the class of square and dark with a row attribute equal to the first loop index
             if(alternateColor) {
               var square = $('<div>').addClass('square dark').attr('row' , i).attr("col", j) ;
+              // switch alternateColor to falsey value
               alternateColor = 1 - alternateColor;
+              // append the square to the current row
               row.append(square);
              } else {
+                 // if falsey, create a div with the class of square and light with a row attribute equal to the first loop index
               var square = $('<div>').addClass('square light').attr('row' , i).attr("col", j) ;
               alternateColor = 1 - alternateColor;
               row.append(square);
              }
-
-          }
-          if ( i>=4 && i <=5) {
-            if(alternateColor) {
-              var square = $('<div>').addClass('square dark').attr('row' , i).attr("col", j) ;
-              alternateColor = 1 - alternateColor;
-              row.append(square);
-             } else {
-              var square = $('<div>').addClass('square light').attr('row' , i).attr("col", j) ;
-              alternateColor = 1 - alternateColor;
-              row.append(square);
-             }
-
-          } else if (i > 5) {
-          
-           if(alternateColor) {
-            var square = $('<div>').addClass('square dark').attr('row' , i).attr("col", j) ;
-            
-            alternateColor = 1 - alternateColor;
-           
-            row.append(square);
-           } else {
-            var square = $('<div>').addClass('square light').attr('row' , i).attr("col", j) ;
-            alternateColor = 1 - alternateColor;
-            row.append(square);
-           }
         }
-        gameBoard.append(row);
+        //target the class of gameArea to append the row
+        $('#gameArea').append(row);
+        // alternate the value of alternateColor to make the next row start with opposite color div
+        alternateColor = 1 - alternateColor;
     } 
-    alternateColor = 1 - alternateColor;
+  
   }
-}
-
 
 function repopulateChecker(){
-    var gameBoard = $('#gameArea');
-    for(var i = 0; i < gameboard.length; i++){
-        for(var j =0; j < gameboard.length; j++){
-            if(gameboard[i][j]=== 1){
+    // loop through the gameBoard array
+    for(var i = 0; i < gameBoard.length; i++){
+        //loop though easch number in the inner array
+        for(var j =0; j < gameBoard.length; j++){
+            // if the gameBoard value at the outerloop index and innerloop index is a 1 the append a checker with the class of player1
+            if(gameBoard[i][j]=== 1){
                 var checker = $('<div>').addClass('checker player1');
+                //jQuery target the div with the row attribute of the outer loop index and col attribute of inner loop index and append the checker
                 $(`div[row = ${i}][col= ${j}]`).append(checker);
             }
-            else if(gameboard[i][j]=== 2){
+            // if the gameBoard value at the outerloop index and innerloop index is a 2 the append a checker with the class of player2
+            else if(gameBoard[i][j]=== 2){
                 var checker = $('<div>').addClass('checker player2');
                 $(`div[row = ${i}][col= ${j}]`).append(checker);
             }
@@ -106,13 +91,9 @@ function repopulateChecker(){
     switchPlayer();
 }
 
-function movePiece(){
-    var row = parseInt($(this).attr('row'));
-    var column = parseInt($(this).attr('col'));
-}
-
 function resetGame() {
-    gameboard = [
+    // set the gameBoard to the original status
+    gameBoard = [
         [0,1,0,1,0,1,0,1],
         [1,0,1,0,1,0,1,0],
         [0,1,0,1,0,1,0,1],
@@ -122,42 +103,55 @@ function resetGame() {
         [0,2,0,2,0,2,0,2],
         [2,0,2,0,2,0,2,0],
       ];
+    // remove all the children div's (checkers) from the divs with the class of square
       $('.square').empty();
+    // reloop through array an assign checkers to original position
       repopulateChecker();
 }
 
 function selectPiece(){
-    
+    // has a div has not been clicked
     if(!selectedChecker){
         console.log(this);
-      
+        //get the numeric value of the div's row attribute that was clicked
         currentRow = parseInt($(this).attr('row'));
+        //get the numeric value of the div's col attribute that was clicked
         currentColumn = parseInt($(this).attr('col'));
+        //check who the current player is
         if (currentPlayer) {
+            // check the possible movements of the piece
             possibleRow = currentRow + 1;
             possibleMoveLeft = currentColumn + 1;
             possibleMoveRight = currentColumn - 1; 
         } else {
+            // check the possible movements of the pieces from player 2's perspective
             possibleRow = currentRow - 1;
             possibleMoveLeft = currentColumn -1;
             possibleMoveRight = currentColumn + 1; 
         }
+        //jQuery target the div with the row and col value equal to the current checkers possible moves and add the class highLight
         $(`div[row = ${possibleRow}][col = ${possibleMoveLeft}]`).addClass("highLight");
         $(`div[row= ${possibleRow}][col = ${possibleMoveRight}]`).addClass("highLight");
-        selectedChecker = gameboard[currentRow][currentColumn];
+        selectedChecker = gameBoard[currentRow][currentColumn];
     } else {
+        //if the next square clicked has the class of highlight
         if($(this).hasClass('highLight')){
+            //reset the checker so another checker may be selected
             selectedChecker = null;
+            //jQuery target the div with the row and col value equal to the current checkers possible moves and remove the class highLight
             $(`div[row = ${possibleRow}][col = ${possibleMoveLeft}]`).removeClass("highLight");
             $(`div[row= ${possibleRow}][col = ${possibleMoveRight}]`).removeClass("highLight");
+            //find the numeric value of row attribute the highlighted div clicked
             var moveToRow = parseInt($(this).attr('row'));
+            //find the numeric value of col attribute the highlighted div clicked
             var moveToColumn = parseInt($(this).attr('col'));
+            //check which player is doing the action to determine which color checker
                 if(currentPlayer) {
-                    gameboard[moveToRow][moveToColumn] = 1;
+                    gameBoard[moveToRow][moveToColumn] = 1;
                 } else {
-                    gameboard[moveToRow][moveToColumn] = 2;
+                    gameBoard[moveToRow][moveToColumn] = 2;
                 }
-            gameboard[currentRow][currentColumn]= 0;
+            gameBoard[currentRow][currentColumn]= 0;
             $('.square').empty();
             repopulateChecker();
         } else {
